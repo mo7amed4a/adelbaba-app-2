da27ad4082562df83e10f80427869b90066fe418
mo7amed4a <m7md0a99@gmail.com>
2024-11-05 15:17:32 +0200

    first

---
+++ app/[lng]/second-page/page.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import { useTranslation } from '../../i18n'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
+
+export default async function Page({ params }: {
+  params: {
+    lng: string;
+  };
+}) {
+  let { lng } = await params
+  const { t } = await useTranslation(lng, 'second-page')
+  return (
+    <>
+      <main>
+        <Header
+          heading={t('h1')}
+        />
+        <Link href={`/${lng}`}>
+          <button type="button">
+            {t('back-to-home')}
+          </button>
+        </Link>
+      </main>
+      <Footer lng={lng} path="/second-page" />
+    </>
+  )
+}
---
+++ app/components/layouts/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FooterApp() {
+  return (
+    <div>FooterApp</div>
+  )
+}
