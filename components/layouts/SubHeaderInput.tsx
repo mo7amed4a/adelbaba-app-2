277f48df460b346e4efc09da7937ab3c52378563
mo7amed4a <m7md0a99@gmail.com>
2024-11-30 12:56:01 +0200

    edit

--- components/layouts/SubHeaderInput.tsx
+++ components/layouts/SubHeaderInput.tsx
@@ -4,7 +4,7 @@ import InputWithIcon from "../global/InputIcon";
 export default function SubHeaderInput() {
     return (
       <>
-        <div className="flex w-full bg-red-400 max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12 justify-center">
+        <div className="flex w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12 justify-center">
           <InputWithIcon
             className="bg-white"
             startIcon={<Search />}
